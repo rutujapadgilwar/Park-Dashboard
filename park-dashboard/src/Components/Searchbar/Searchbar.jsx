0f5a324dc9617bbbcfc1d424b999f9bbb60695c8
Shreya Surendra Choure <schoure@pdx.edu>
2023-05-18 17:41:02 -0700

    searchbar dvelopment started

---
+++ park-dashboard/src/Components/Searchbar/Searchbar.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import Select from "react-select";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import "./searchbar.css"
+
+const BASE_URL = "https://developer.nps.gov/api/v1";
+const API_KEY = "OaR7jmqSa22JAcsym9lVfStp58LmCqH9JdZUPEH7";
+
+const Searchbar = () => {
+  const [parkName, setParkName] = useState([]);
+
+  useEffect(() => {
+    const fetchNames = async () => {
+      try {
+        const response = await axios.get(`${BASE_URL}/parks`, {
+          params: {
+            api_key: API_KEY,
+            limit: 500,
+          },
+        });
+        const parks = response.data.data;
+        const nationalParks = parks.filter(
+          (park) =>
+            park.designation === "National Park" &&
+            park.fullName !== "Virgin Islands National Park"
+        );
+        const parkNames = nationalParks.map((park) => ({
+          fullName: park.fullName,
+          parkCode: park.parkCode,
+        }));
+        setParkName(parkNames);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchNames();
+  }, []);
+
+  const options = parkName.map((park) => ({
+    value: park.fullName,
+    label: (
+      <Link className="links"
+        to={`/parkInfoPage/${park.parkCode}`}
+      >
+        {park.fullName}
+      </Link>
+    ),
+  }));
+
+  return (
+    <div className="searchbar">
+
+      <Select
+        className="searchbar-comp"
+        options={options}
+        placeholder="Search for National Park"
+        id="park-name" 
+      />
+
+    </div>
+  );
+};
+
+export default Searchbar;
--- park-dashboard/src/Pages/Home/HomePage.js
+++ park-dashboard/src/Pages/Home/HomePage.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Searchbar from "../../components/Searchbar/Searchbar";
 import StatesData from "../../components/NationalParksMap/StatesData";
 const HomePage = () => {
   return (
     <div>
+      <Searchbar className="py-5" />
       <StatesData />
     </div>
   );
