0ad1d27d2946e9fcc508da7d6338e201cbc2e243
Shreya Surendra Choure <schoure@pdx.edu>
2023-05-23 01:02:45 -0700

    Campgrounds component started

---
+++ park-dashboard/src/Components/Campgrounds/Campgrounds.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const BASE_URL = "https://developer.nps.gov/api/v1";
+const API_KEY = "OaR7jmqSa22JAcsym9lVfStp58LmCqH9JdZUPEH7";
+
+export const Campgrounds = () => {
+	const [campgrounds, setCampgrounds] = useState([]);
+
+	useEffect(() => {
+		const cacheKey = "campgrounds-data";
+		const fetchCampgrounds = async () => {
+			const cacheData = localStorage.getItem(cacheKey);
+			if (cacheData) {
+				setCampgrounds(JSON.parse(cacheData));
+			} else {
+				try {
+					const response = await axios.get(`${BASE_URL}/campgrounds`, {
+						params: {
+							api_key: API_KEY,
+							limit: 700,
+						},
+					});
+
+					setCampgrounds(response.data.data);
+
+					localStorage.setItem(cacheKey, JSON.stringify(response.data.data));
+				} catch (error) {
+					console.error(error);
+				}
+			}
+		};
+		fetchCampgrounds();
+	}, []);
+	return campgrounds;
+};
--- park-dashboard/src/Pages/Generic Charts/GenericCharts.js
+++ park-dashboard/src/Pages/Generic Charts/GenericCharts.js
@@ -1,5 +1,11 @@
 import React from "react";
+import CampgroundsChart from "../../Components/Campgrounds/CampgroundsChart";
+
 const GenericChart = () => {
-  return <div></div>;
+	return (
+		<div style={{marginTop: "6rem"}}>
+			<CampgroundsChart className="py-5" />
+		</div>
+	);
 };
 export default GenericChart;
