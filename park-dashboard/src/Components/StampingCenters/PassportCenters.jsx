2744d6b560769b84cbe4e8518b26e29dfd07e0bc
Shreya Surendra Choure <schoure@pdx.edu>
2023-05-30 18:12:19 -0700

    List of centers added

--- park-dashboard/src/Components/StampingCenters/PassportCenters.jsx
+++ park-dashboard/src/Components/StampingCenters/PassportCenters.jsx
@@ -1,20 +1,43 @@
 import React from "react";
 import { StampingList } from "./StampingList";
+import "./stamping.css";
+import stampImage from "./stampLogo.png";
 
 const PassportCenters = (centers) => {
   const centersData = StampingList(centers);
   const parkCode = centers.parkCode;
-  const center = centersData.find((c) => c.parks[0].parkCode === parkCode);
+  let stampingCenters = [];
+
+  for (let i = 0; i < centersData.length; i++) {
+    const center = centersData[i];
+    const parks = center.parks;
+
+    for (let j = 0; j < parks.length; j++) {
+      const park = parks[j];
+
+      if (park.parkCode === parkCode) {
+        stampingCenters.push(center);
+        break;
+      }
+    }
+  }
+
   return (
-    <div className="box">
-      <h2>Passport Stamping</h2>
-      {center ? (
+    <div className="passport-locations">
+      <div className="stamping-image">
+      <img className="stamp" src={stampImage} alt="Stamping-Logo" />
+      </div>
+      <div className="location-list">
+      {stampingCenters.length > 0 ? (
         <ul>
-          <li>{center.label}</li>
+          {stampingCenters.map((center) => (
+            <div className="center-name"><li key={center.label}>{center.label}</li></div>
+          ))}
         </ul>
       ) : (
-        <p>Center not found.</p>
+        <p>No stamping center found.</p>
       )}
+      </div>
     </div>
   );
 };
