a49a452848566abf7bab1947e0ac0c3d74cfced1
juhi <juh3@pdx.edu>
2023-05-30 22:45:35 -0700

    Made doughnut chart responsive

--- park-dashboard/src/Pages/NationalParkCharts/NationalParkChartsPage.js
+++ park-dashboard/src/Pages/NationalParkCharts/NationalParkChartsPage.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import ActivitiesVsNP from "../../Components/NationalParkActivities/ActivitiesVsNP";
 
 const NationalParkChartPage = () => {
-  return <ActivitiesVsNP />;
+  return (
+    <Container style={{ justifyContent: "space-around" }}>
+      <Row>
+        <Col md={6}>
+          <div style={{ width: "45vw", height: "45vh", paddingTop: "70px" }}>
+            <h1 className="text-center" style={{ marginBottom: "-60px" }}>
+              Activities vs National Parks
+            </h1>
+            <ActivitiesVsNP />
+          </div>
+        </Col>
+        <Col md={6}>
+          <div style={{ width: "45vw", height: "45vh", paddingTop: "70px" }}>
+            <h1 className="text-center" style={{ marginBottom: "-60px" }}>
+              Activities vs National Parks
+            </h1>
+            <ActivitiesVsNP />
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 export default NationalParkChartPage;
